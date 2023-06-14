4165897cd40803883a15784f9973bcec35163d9f
Akash Upadhyay <Akash.upadhyay>
2023-06-14 12:06:52 +0530

    removed links

--- index.js
+++ index.js
@@ -19,23 +19,7 @@ let currentBranch = 'master'
 // let currentBranch = shell.exec(`git branch --show-current`).stdout
 
 let gitLabUrls = [
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_user.git", fileName: "platform_user" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_kyc.git", fileName: "platform_kyc" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_redeem.git", fileName: "platform_redeem" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/chatbot.git", fileName: "chatbot" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_lobby.git", fileName: "platform_lobby" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_razorpay.git", fileName: "platform_razorpay" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/justpay-pg.git", fileName: "justpay" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_payment_gateways.git", fileName: "platform_payment_gateways" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_onfido_kyc.git", fileName: "platform_onfido_kyc" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/acepoints.git", fileName: "acepoints" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_clevertap.git", fileName: "platform_clevertap" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_matchmaking.git", fileName: "platform_matchmaking" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_pocker.git", fileName: "platform_pocker" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_game.git", fileName: "platform_game" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_refer_earn.git", fileName: "platform_refer_earn" },
-  { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/platform_gps.git", fileName: "platform_gps" },
-  // { gitUrl: "http://gitlab.corp.hdworks.in/platformbackend/commonServices.git", fileName: "commonServices" },
+  
 ]
 
 async function welcome() { console.log(gradient.pastel("Serverless Deploy CLI")) }
@@ -166,24 +150,7 @@ async function askToSelectProjects() {
     name: 'selectProjects',
     type: 'checkbox',
     message: `Select Projects you want to deploy\n`,
-    // choices: [
-      // "platform_user",
-      // "platform_kyc",
-      // "platform_redeem",
-      // "chatbot",
-      // "platform_lobby",
-      // "platform_payment_gateways",
-      // "platform_onfido_kyc",
-      //   "platform_clevertap",
-      //   "acepoints",
-      //   "platform_matchmaking",
-      //   "platform_pocker",
-      //   "platform_game",
-        // "platform_refer_earn",
-        // "platform_gps",
-        // "commonServices"
-    // ]
-    // choices: gitLabUrls
+    
     choices: gitLabUrls
   });
 
